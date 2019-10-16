436f4f64c245c25edaa2a44519a3f602bece3f50
April Long <april11dance@gmail.com>
2019-10-15 20:32:46 -0600

    changes made in all files

---
+++ controller/burgers_controllers.js
@@ -0,0 +1,32 @@
+var express= require("express");
+var router= express.Router();
+var burgers= require(".../models/burger.js");
+
+router.get("/", function(req, res){
+
+    res.redirect("burgers")
+
+}); 
+
+router.post("/burgers/create", function(req,res){
+	burgers.insertOne([
+		"burger_name"
+		],[
+			req.body.burger_name
+			], function(data){
+				res.redirect("/burgers");
+			});
+});
+
+router.put("/burgers/update/:id", function(req,res){
+	var condition = "id = " + req.params.id;
+	console.log("condition", condition);
+
+	burgers.updateOne({
+		"devoured": req.body.devoured
+	}, condition, function(data){
+		res.redirect("/burgers")
+	});
+});
+
+module.exports = router;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name) VALUES ('Cheeseburger', true);
+INSERT INTO burgers (name) VALUES ('Plant-based', false);
+INSERT INTO burgers (name, sleepy) VALUES ('Bacon burger', true);
